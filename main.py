d4111cd34c635f0e9dd1be710c38b6ffb603a447
gi-b716 <sganyunchi@163.com>
2023-02-18 12:58:24 +0800

    v1.0.0 d2 添加功能菜单，目前可以选择3个功能 优化“连接测试”模块

--- main.py
+++ main.py
@@ -1,6 +1,36 @@
 import module_connection_test
 
-print("-----------------------------")
-print("modrinth小助手 —— GavinCQTD")
-print("v1.0.0 d1")
-print("-----------------------------")
+
+function = ""
+
+
+def main():
+    global function
+
+    print("-----------------------------")
+    print("请选择功能：    (输入功能编号)")
+    print("1.api连接测试")
+    print("2.关于\n")
+    print("q.退出程序")
+    print("-----------------------------")
+    
+    function = input("")
+    if function=="1":
+        connection_test()
+    elif function=="2":
+        about()
+    
+def connection_test():
+    print(module_connection_test.CTt())
+
+def about():
+    print("-----------------------------")
+    print("modrinth小助手 —— GavinCQTD")
+    print("v1.0.0 d2")
+    print("-----------------------------\n")
+
+
+about()
+while function!="q":
+    main()
+print("------感谢使用！------")
--- module_connection_test.py
+++ module_connection_test.py
@@ -5,6 +5,8 @@ def CTt():
     getsj = gets.json()
     
     if gets.status_code == 200:
-        print("连接成功！modrinth api版本：{0}".format(getsj['version']))
+        info = "连接成功！modrinth api版本：{0}".format(getsj['version'])
     else:
-        print("连接出现错误。状态码：{0}".format(gets.status_code))
+        info = "连接出现错误。状态码：{0}".format(gets.status_code)
+    
+    return info
